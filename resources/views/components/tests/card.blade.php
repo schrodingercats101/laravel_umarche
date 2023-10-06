55f8eb856a6966a82cab0eb0a6dc135a99419d38
schrodingercats101 <k4290224@gmail.com>
2023-10-06 16:17:35 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+    'message' => '初期値',
+    'title' => 'タイトル初期値',
+    'content' => '本文初期値'
+    ])
+
 <div class="border shadow-md w-1/4 p-2">
     <div>{{ $title }}</div>
     <div>画像</div>
