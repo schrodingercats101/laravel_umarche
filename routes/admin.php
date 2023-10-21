d266cccdde1655750cfc8a3fd192f514a687ffd7
schrodingercats101 <k4290224@gmail.com>
2023-10-22 08:11:05 +0900

    softDelete

--- routes/admin.php
+++ routes/admin.php
@@ -27,6 +27,14 @@ Route::get('/', function () {
     return view('admin.welcome');
 });
 
+Route::prefix('expired-owners')->
+middleware('auth:admin')->group(function(){
+    Route::get('index', [OwnersController::class, 'expiredOwnerIndex'])->name('expired-owners.index');
+    Route::post('destroy/{owner}', [OwnersController::class, 'expiredOwnerDestroy'])->name('expired-owners.destroy');
+});
+
+
+
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->middleware(['auth:admin'])->name('dashboard');
