ba42d252db5dec06bd4480c0d9db12ba79511fc6
schrodingercats101 <k4290224@gmail.com>
2023-10-22 10:05:08 +0900

    ルート、ビュー編集

--- routes/admin.php
+++ routes/admin.php
@@ -23,9 +23,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.welcome');
-});
+// Route::get('/', function () {
+//     return view('admin.welcome');
+// });
 
 Route::prefix('expired-owners')->
 middleware('auth:admin')->group(function(){
@@ -40,7 +40,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth:admin'])->name('dashboard');
 
 Route::resource('owners', OwnersController::class)
-->middleware('auth:admin');
+->middleware('auth:admin')->except(['show']);
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
