570646f5d97a7466d3cea286026dabda5ce206ff
schrodingercats101 <k4290224@gmail.com>
2023-10-29 11:47:56 +0900

    リサイズなしアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
 
 class ShopController extends Controller
 {
@@ -14,35 +16,48 @@ class ShopController extends Controller
         $this->middleware('auth:owners');
 
         $this->middleware(function ($request, $next) {
-
             // dd($request->route()->parameter('shop')); //文字列
             // dd(Auth::id()); //数字
 
-                $id = $request->route()->parameter('shop'); //shopのid取得
-                if(!is_null($id)){ // null判定
-                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
-                $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換 $ownerId = Auth::id();if($shopId !== $ownerId){ // 同じでなかったら
-                abort(404); // 404画面表示 }
+            $id = $request->route()->parameter('shop'); //shopのid取得
+            if(!is_null($id)){ // null判定
+            $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換
+                $ownerId = Auth::id();
+                if($shopId !== $ownerId){ // 同じでなかったら
+                    abort(404); // 404画面表示
                 }
+            }
             return $next($request);
         });
-
     }
 
     public function index()
     {
-        // $ownerId = Auth::id(); // 認証されているid
-        $shops = Shop::where('owner_id',  Auth::id())->get(); // whereは検索条件
 
-        return view('owner.shops.index',compact('shops'));
+        //$ownerId = Auth::id();
+        $shops = Shop::where('owner_id', Auth::id())->get();
+
+        return view('owner.shops.index',
+        compact('shops'));
     }
+
     public function edit($id)
     {
-
+        $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+        return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
-
+        $imageFile = $request->image; //一時保存
+        if(!is_null($imageFile) && $imageFile->isValid() )
+        {
+        Storage::putFile('public/shops', $imageFile);
+        }
+        return redirect()->route('owner.shops.index');
     }
+
+
 }
